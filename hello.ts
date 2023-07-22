3b8dd2fddb21af36c0b616ad4678d7c24d3c2512
Moeedahmad899 <70706892+Moeedahmad899@users.noreply.github.com>
2023-07-22 23:24:38 +0500

    first assignment uploaded

---
+++ hello.ts
@@ -0,0 +1,6 @@
+// Store the person's name in a variable
+let personName: string = "Eric";
+
+// Print a message to the person
+let message: string = `Hello ${personName}, would you like to learn some TypeScript today?`;
+console.log(message);
