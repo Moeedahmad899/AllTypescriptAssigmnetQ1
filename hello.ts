fe5fdab3a43a09503b5d3f96f591e839c006e2e9
Moeedahmad899 <70706892+Moeedahmad899@users.noreply.github.com>
2023-09-02 12:06:39 +0500

    Delete hello.ts

--- hello.ts
+++
@@ -1,6 +0,0 @@
-// Store the person's name in a variable
-let personName: string = "Eric";
-
-// Print a message to the person
-let message: string = `Hello ${personName}, would you like to learn some TypeScript today?`;
-console.log(message);
